250cf17d42609a739d64e16fbb1858dddf87b3ea
Jumana Ahmed Khawaji <jumanaAhmedCS@outlook.com>
2025-10-28 12:45:49 +0300

    Create FocusSession view with get and put methods (retrive and update focus session)

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Homepage, FocusLogsIndex, TagsIndex
+from .views import Homepage, FocusLogsIndex, FocusLogSession, TagsIndex
 urlpatterns = [
     path('', Homepage.as_view(), name='homepage'),
     path('focusLogs/', FocusLogsIndex.as_view(), name='focusLogs_index'),
+    path('focusLogs/<int:session_id>/', FocusLogSession.as_view(), name='focusLog_session'),
     path('tags/', TagsIndex.as_view(), name='tags_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from .models import FocusLog, Tag
 from .serializers import FocusLogSerializer, TagSerializer
 
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 class Homepage(APIView):
     def get(self, request):
@@ -23,6 +25,27 @@ class FocusLogsIndex(APIView):
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class FocusLogSession(APIView):
+    def get(self, request, session_id):
+        try:
+            queryset = get_object_or_404(FocusLog, id=session_id)
+            serializer = FocusLogSerializer(queryset)
+            return Response(serializer.data)
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def put(self, request, session_id):
+        try:
+            queryset = get_object_or_404(FocusLog, id=session_id)
+            serializer = FocusLogSerializer(queryset, data=request.data)
+            
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as error:
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
