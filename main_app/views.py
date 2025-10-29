0c2796930045873314fe958711908b872f1fd0c8
Jumana Ahmed Khawaji <jumanaAhmedCS@outlook.com>
2025-10-29 12:57:49 +0300

    Update ToDoListDetail view with put method to update an exsiting todolist

--- main_app/views.py
+++ main_app/views.py
@@ -133,3 +133,15 @@ class ToDoListDetail(APIView):
             return Response(serializer.data)
         except Exception as error:
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def put(self, request, todolist_id):
+        try:
+            queryset = get_object_or_404(ToDoList, id=todolist_id)
+            serializer = ToDoListSerializer(queryset, data=request.data)
+            
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
