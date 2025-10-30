c79624170016abd28f36f41292c4f2e7b9f054b8
Jumana Ahmed Khawaji <jumanaAhmedCS@outlook.com>
2025-10-30 07:24:13 +0300

    Create TasksIndex view with get method to retrive all todolist's tasks, and post method to create new task in the todolist

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Homepage, FocusLogsIndex, FocusLogSession, TagsIndex, AssignDistraction, UnassignDistraction, ToDoListsIndex, ToDoListDetail
+from .views import Homepage, FocusLogsIndex, FocusLogSession, TagsIndex, AssignDistraction, UnassignDistraction, ToDoListsIndex, ToDoListDetail, TasksIndex
 urlpatterns = [
     path('', Homepage.as_view(), name='homepage'),
     path('focusLogs/', FocusLogsIndex.as_view(), name='focusLogs_index'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('focusLogs/<int:session_id>/assignDistraction/<int:distraction_id>/', AssignDistraction.as_view(), name='assign_distraction'), 
     path('focusLogs/<int:session_id>/unassignDistraction/<int:distraction_id>/', UnassignDistraction.as_view(), name='unassign_distraction'),
     path('toDoLists/', ToDoListsIndex.as_view(), name='toDoLists_index'),
-    path('toDoLists/<int:todolist_id>/', ToDoListDetail.as_view(), name='toDoList_detail')
+    path('toDoLists/<int:todolist_id>/', ToDoListDetail.as_view(), name='toDoList_detail'),
+    path('toDoLists/<int:todolist_id>/tasks/', TasksIndex.as_view(), name='tasks_index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import FocusLog, Tag, Distraction, ToDoList
-from .serializers import FocusLogSerializer, TagSerializer, DistractionSerializer, ToDoListSerializer
+from .models import FocusLog, Tag, Distraction, ToDoList, Task
+from .serializers import FocusLogSerializer, TagSerializer, DistractionSerializer, ToDoListSerializer, TaskSerializer
 
 from django.shortcuts import get_object_or_404
 
@@ -153,3 +153,22 @@ class ToDoListDetail(APIView):
             return Response({'message': f'Your todolist with id #{todolist_id} has been deleted'}, status=status.HTTP_204_NO_CONTENT)
         except Exception as error:
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class TasksIndex(APIView):
+    def get(self, request, todolist_id):
+        try:
+            queryset = Task.objects.filter(todolist=todolist_id)
+            serializer = TaskSerializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            
+    def post(self, request, todolist_id):
+        serializer = TaskSerializer(data = request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            queryset = Task.objects.filter(todolist=todolist_id)
+            serializer= TaskSerializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
