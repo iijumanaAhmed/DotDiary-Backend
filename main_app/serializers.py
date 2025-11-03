15a658c1c95f9e20e796ab6a2582cec7a7204270
Jumana Ahmed Khawaji <jumanaAhmedCS@outlook.com>
2025-11-03 13:51:13 +0300

    Add UserSerializer

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 from .models import Task, ToDoList, Distraction, Tag, FocusLog
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= User
+        fields = [ 'username', 'email', 'first_name', 'last_name']
+
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
